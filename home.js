ae4f920b64593b8bf1c42c64a8489e8d7ea8d642
dracmakesbals <131245369+dracmakesbals@users.noreply.github.com>
2024-02-06 18:09:17 +0530

    the home page is added

---
+++ home.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".ham_menu");
+const nav_links = document.querySelector(".nav_links");
+const li = document.querySelectorAll(".nav_links li");
+
+hamburger.addEventListener("click", () => {
+  nav_links.classList.toggle("open");
+  li.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+});
